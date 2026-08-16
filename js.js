35ac07e0009f0114261ea964a0e4bd698f153466
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-16 14:02:25 +0100

    class and unshift new class to array

--- js.js
+++ js.js
@@ -1,11 +1,27 @@
 const save = document.getElementById("save");
 
+class ObjectEntry {
+    constructor (title, thought){
+    this.title = title,
+    this.thought = thought
+    }
+};
+let newEntries = [];
+
 function saveThought(){
 
 const title = document.getElementById("newThoughtTitle").value;
 const thought = document.getElementById("newThought").value;
 
-console.log(`${title}: ${thought}`);
+//Create object
+const newEntry = new ObjectEntry(title, thought);
+
+//unshift to array
+newEntries.unshift(newEntry);
+
+
+//console.log(`${title}: ${thought}`);
+localStorage.setItem('entries', JSON.stringify(newEntries));
 };
 
-save.addEventListener("click", saveThought);
+save.addEventListener("click", saveThought); //Remove the parentheses stops being accidently called before
