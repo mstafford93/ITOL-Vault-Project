693d15447ce3722deb125f334664ef7ef22ecd62
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-16 14:09:50 +0100

    alert for black thought

--- js.js
+++ js.js
@@ -10,18 +10,27 @@ let newEntries = [];
 
 function saveThought(){
 
+
 const title = document.getElementById("newThoughtTitle").value;
 const thought = document.getElementById("newThought").value;
 
+if (title == "" ||  thought == ""){
+    alert("Please enter thought and title");
+} else {
 //Create object
 const newEntry = new ObjectEntry(title, thought);
 
 //unshift to array
 newEntries.unshift(newEntry);
 
-
 //console.log(`${title}: ${thought}`);
 localStorage.setItem('entries', JSON.stringify(newEntries));
+
+//Resets inputs
+document.getElementById("newThoughtTitle").value = "";
+document.getElementById("newThought").value = "";
+}
+
 };
 
 save.addEventListener("click", saveThought); //Remove the parentheses stops being accidently called before
